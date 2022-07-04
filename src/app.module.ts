c956cd2f7a695518ba422f24072b2e0f50245460
Maxim <maksim1337.ru@mail.ru>
2022-07-04 14:59:48 +0700

    with env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,21 +6,36 @@ import { VisitModule } from './visits/visit.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import * as Joi from 'joi';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 
 @Module({
-  imports: [VisitModule, UserModule, AuthModule, 
-    MongooseModule.forRoot(`mongodb://localhost:27017/visits`,{ useNewUrlParser: true } ),
-     ConfigModule.forRoot({
-    validationSchema: Joi.object({
-      JWT_SECRET: Joi.string().required(),
-      JWT_EXPIRATION_TIME: Joi.string().required(),
-    })
-  })],
+  imports: [VisitModule, UserModule, AuthModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_DATABASE: Joi.string().required(),
+        MONGO_HOST: Joi.string().required(),
+        MONGO_PORT: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST');
+        const port = configService.get('MONGO_PORT');
+
+
+        return {
+          uri: `mongodb://${host}:${port}`,
+          dbName: database,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
-//MongooseModule.forRoot(`mongodb://localhost:27017/visits`,{ useNewUrlParser: true } )
+export class AppModule { }
